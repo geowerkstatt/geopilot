9c277e51187cfa27b1ab39c1ba6ff82ff346d53e
Philipp Lüthi <philipp.luethi@geowerkstatt.ch>
2023-10-18 17:34:02 +0200

    Add Api.Tests Project

---
+++ tests/GeoCop.Api.Test/VersionConstrollerTest.cs
@@ -0,0 +1,23 @@
+﻿namespace GeoCop.Api.Controllers
+{
+    [TestClass]
+    public class VersionConstrollerTest
+    {
+        private VersionController controller;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            controller = new VersionController();
+        }
+
+        [TestMethod]
+        public void GetVersion()
+        {
+            var result = controller.Get();
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Length > 0);
+            Assert.AreEqual("1.0.0", result);
+        }
+    }
+}
