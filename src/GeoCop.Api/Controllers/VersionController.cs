55d8c8a5e4e85312fe795a868ce988247c9aa86b
Philipp Lüthi <philipp.luethi@geowerkstatt.ch>
2023-10-18 16:48:10 +0200

    Replace weather api with version api
    Remove boilerplate styling

---
+++ src/GeoCop.Api/Controllers/VersionController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace GeoCop.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VersionController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            var assembly = typeof(Program).Assembly;
+            return assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion ??
+                assembly.GetName()?.Version?.ToString() ?? String.Empty;
+        }
+    }
+}
--- src/GeoCop.Frontend/src/App.jsx
+++ src/GeoCop.Frontend/src/App.jsx
@@ -5,55 +5,38 @@ export default class App extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { forecasts: [], loading: true };
+        this.state = { version: "Unknown", loading: true };
     }
 
     componentDidMount() {
-        this.populateWeatherData();
+        this.fetchVersion();
     }
 
-    static renderForecastsTable(forecasts) {
+    static renderVersionInfo(version) {
         return (
-            <table className='table table-striped' aria-labelledby="tabelLabel">
-                <thead>
-                    <tr>
-                        <th>Date</th>
-                        <th>Temp. (C)</th>
-                        <th>Temp. (F)</th>
-                        <th>Summary</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {forecasts.map(forecast =>
-                        <tr key={forecast.date}>
-                            <td>{forecast.date}</td>
-                            <td>{forecast.temperatureC}</td>
-                            <td>{forecast.temperatureF}</td>
-                            <td>{forecast.summary}</td>
-                        </tr>
-                    )}
-                </tbody>
-            </table>
+            <p>Current Version {version}</p>
         );
     }
 
     render() {
         let contents = this.state.loading
             ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-            : App.renderForecastsTable(this.state.forecasts);
+            : App.renderVersionInfo(this.state.version);
 
         return (
             <div>
-                <h1 id="tabelLabel" >Weather forecast</h1>
+                <h1 id="tabelLabel">GeoCop</h1>
                 <p>This component demonstrates fetching data from the server.</p>
                 {contents}
             </div>
         );
     }
 
-    async populateWeatherData() {
-        const response = await fetch('weatherforecast');
-        const data = await response.json();
-        this.setState({ forecasts: data, loading: false });
+    async fetchVersion() {
+        const response = await fetch('api/Version');
+        const data = await response.text();
+        if (data.length == 0)
+            return;
+        this.setState({ version: data, loading: false });
     }
 }
