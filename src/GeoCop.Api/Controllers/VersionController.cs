8d109c32969541f94d6120ec3fab90b8020182bf
Philipp Lüthi <philipp.luethi@geowerkstatt.ch>
2023-10-18 17:32:18 +0200

    Add .editorconfig/.gitattributes

--- src/GeoCop.Api/Controllers/VersionController.cs
+++ src/GeoCop.Api/Controllers/VersionController.cs
@@ -5,14 +5,18 @@ namespace GeoCop.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class VersionController : ControllerBase
+    internal class VersionController : ControllerBase
     {
+        /// <summary>
+        /// Gets the application version.
+        /// </summary>
+        /// <returns>Version string.</returns>
         [HttpGet]
         public string Get()
         {
             var assembly = typeof(Program).Assembly;
             return assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion ??
-                assembly.GetName()?.Version?.ToString() ?? String.Empty;
+                assembly.GetName()?.Version?.ToString() ?? string.Empty;
         }
     }
 }
