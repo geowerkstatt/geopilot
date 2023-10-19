58a9ea15f6c88960c1b040e59fab737533c7b7a7
Philipp Lüthi <philipp.luethi@geowerkstatt.ch>
2023-10-19 15:12:29 +0200

    Remove template boilerplate

--- tests/GeoCop.Api.Test/VersionConstrollerTest.cs
+++ tests/GeoCop.Api.Test/VersionConstrollerTest.cs
@@ -3,18 +3,10 @@
     [TestClass]
     public class VersionConstrollerTest
     {
-        private VersionController controller;
-
-        [TestInitialize]
-        public void Initialize()
-        {
-            controller = new VersionController();
-        }
-
         [TestMethod]
         public void GetVersion()
         {
-            var result = controller.Get();
+            var result = new VersionController().Get();
             Assert.IsNotNull(result);
             Assert.IsTrue(result.Length > 0);
             Assert.AreEqual("1.0.0", result);
