306db7954a1f5c9b9e7241885833a2d37bf57dda
Philipp Lüthi <philipp.luethi@geowerkstatt.ch>
2023-10-20 16:02:22 +0200

    Fix App.jsx formatting

--- src/GeoCop.Frontend/src/App.jsx
+++ src/GeoCop.Frontend/src/App.jsx
@@ -21,10 +21,7 @@ export default class App extends Component {
       <p>
         <em>
           Loading... Please refresh once the ASP.NET backend has started. See{" "}
-          <a href="https://aka.ms/jspsintegrationreact">
-            https://aka.ms/jspsintegrationreact
-          </a>{" "}
-          for more details.
+          <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.
         </em>
       </p>
     ) : (
