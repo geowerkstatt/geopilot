26e4a262fe48d25bccf06b1bce65dacebc7171c6
tschumpr <roswita.tschuemperlin@geowerkstatt.ch>
2023-11-10 17:16:14 +0100

    Add tests

---
+++ tests/GeoCop.Api.Test/FileContentTypeProviderTest.cs
@@ -0,0 +1,28 @@
+﻿using GeoCop.Api.StacServices;
+
+namespace GeoCop.Api.Test
+{
+    [TestClass]
+    public class FileContentTypeProviderTest
+    {
+        private FileContentTypeProvider fileContentTypeProvider;
+        [TestInitialize]
+        public void Initialize()
+        {
+            fileContentTypeProvider = new FileContentTypeProvider();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+        }
+
+        [TestMethod]
+        [DataRow(".xtf", "application/interlis+xml")]
+        public void GetContentType(string fileExtension, string expectedMediaType)
+        {
+            var contentType = fileContentTypeProvider.GetContentType(fileExtension);
+            Assert.AreEqual(expectedMediaType, contentType.MediaType);
+        }
+    }
+}
