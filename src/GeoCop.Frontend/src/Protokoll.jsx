49175dc862a7dfe7b8c207ba12789cddac23481d
Dominic Burger <dominic.burger@geowerkstatt.ch>
2023-11-13 10:18:26 +0100

    Move ValidatorResult component to new file

--- src/GeoCop.Frontend/src/Protokoll.jsx
+++ src/GeoCop.Frontend/src/Protokoll.jsx
@@ -1,45 +1,8 @@
 import "./app.css";
+import { ValidatorResult } from "./ValidatorResult";
 import { useState, useRef, useEffect } from "react";
 import DayJS from "dayjs";
 import { Card, Container } from "react-bootstrap";
-import { GoFile } from "react-icons/go";
-
-function getExtension(filename) {
-  const index = filename.lastIndexOf(".");
-  return index === -1 ? "" : filename.substring(index + 1);
-}
-
-const ValidatorResult = ({ jobId, protokollFileName, validatorName, result }) => {
-  const statusClass = result && result.status === "completed" ? "valid" : "errors";
-  const statusText = result && result.status === "completed" ? "Keine Fehler!" : "Fehler!";
-
-  return (
-    <>
-      <hr />
-      <h4>{validatorName}</h4>
-      <p>{result.statusMessage}</p>
-      <Card.Title className={statusClass}>
-        {statusText}
-        <span>
-          {result.logFiles &&
-            Object.entries(result.logFiles).map(([logFileType, logFile]) => (
-              <span key={logFileType} className="icon-tooltip">
-                <a
-                  download={protokollFileName + "." + getExtension(logFile)}
-                  className={statusClass + " download-icon"}
-                  href={`/api/v1/download/${jobId}/${logFile}`}
-                >
-                  <GoFile />
-                  <span className="download-description">{logFileType}</span>
-                </a>
-                <span className="icon-tooltip-text">{logFileType}-Datei herunterladen</span>
-              </span>
-            ))}
-        </span>
-      </Card.Title>
-    </>
-  );
-};
 
 export const Protokoll = ({ log, statusData, fileName, validationRunning }) => {
   const [indicateWaiting, setIndicateWaiting] = useState(false);
