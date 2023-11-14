992c5ae352aab25739d07a840d5bae3d37b7b3c1
Dominic Burger <dominic.burger@geowerkstatt.ch>
2023-11-14 11:56:21 +0100

    Load supported file types from interlis-check-service

--- src/GeoCop.Api/Validation/Interlis/InterlisValidator.cs
+++ src/GeoCop.Api/Validation/Interlis/InterlisValidator.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using System.Globalization;
 using System.Net;
@@ -12,12 +12,14 @@ namespace GeoCop.Api.Validation.Interlis
     public class InterlisValidator : IValidator
     {
         private const string UploadUrl = "/api/v1/upload";
+        private const string SettingsUrl = "/api/v1/settings";
         private static readonly TimeSpan pollInterval = TimeSpan.FromSeconds(2);
 
         private readonly ILogger<InterlisValidator> logger;
         private readonly IFileProvider fileProvider;
         private readonly HttpClient httpClient;
         private readonly JsonSerializerOptions jsonSerializerOptions;
+        private ICollection<string>? supportedFileExtensions;
 
         /// <inheritdoc/>
         public string Name => "ilicheck";
@@ -36,7 +38,12 @@ namespace GeoCop.Api.Validation.Interlis
         /// <inheritdoc/>
         public async Task<ICollection<string>> GetSupportedFileExtensionsAsync()
         {
-            return Array.Empty<string>();
+            if (supportedFileExtensions != null) return supportedFileExtensions;
+
+            var response = await httpClient.GetAsync(SettingsUrl).ConfigureAwait(false);
+            var configResult = await ReadSuccessResponseJsonAsync<IliCheckSettingsResponse>(response, CancellationToken.None).ConfigureAwait(false);
+            supportedFileExtensions = configResult.AcceptedFileTypes?.Split(", ");
+            return supportedFileExtensions ?? Array.Empty<string>();
         }
 
         /// <inheritdoc/>
