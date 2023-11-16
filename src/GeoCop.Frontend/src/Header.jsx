db2ceca8e56cb23eea557b0b6e67574807347da2
Dominic Burger <dominic.burger@geowerkstatt.ch>
2023-11-16 14:43:12 +0100

    Add login and logout to frontend

--- src/GeoCop.Frontend/src/Header.jsx
+++ src/GeoCop.Frontend/src/Header.jsx
@@ -1,20 +1,69 @@
+import { AuthenticatedTemplate, UnauthenticatedTemplate, useMsal } from "@azure/msal-react";
+import { Button } from "react-bootstrap";
+import styled from "styled-components";
 import "./app.css";
 
-export const Header = ({ clientSettings }) => (
-  <header>
-    {clientSettings?.vendor?.logo && (
-      <a href={clientSettings?.vendor?.url} target="_blank" rel="noreferrer">
-        <img
-          className="vendor-logo"
-          src={clientSettings?.vendor?.logo}
-          alt={`Logo of ${clientSettings?.vendor?.name}`}
-          onError={(e) => {
-            e.target.style.display = "none";
-          }}
-        />
-      </a>
-    )}
-  </header>
-);
+const AccountContainer = styled.div`
+  align-items: center;
+  display: flex;
+`;
+
+const AccountNameContainer = styled.span`
+  margin-right: 10px;
+`;
+
+const LoginButton = styled(Button)`
+  font-family: "Dosis", sans-serif;
+`;
+
+export const Header = ({ clientSettings }) => {
+  const { instance } = useMsal();
+  const activeAccount = instance.getActiveAccount();
+
+  async function login() {
+    try {
+      const result = await instance.loginPopup({
+        scopes: clientSettings?.authScopes,
+      });
+      instance.setActiveAccount(result.account);
+    } catch (error) {
+      console.warn(error);
+    }
+  }
+
+  async function logout() {
+    try {
+      await instance.logoutPopup();
+    } catch (error) {
+      console.warn(error);
+    }
+  }
+
+  return (
+    <header>
+      {clientSettings?.vendor?.logo && (
+        <a href={clientSettings?.vendor?.url} target="_blank" rel="noreferrer">
+          <img
+            className="vendor-logo"
+            src={clientSettings?.vendor?.logo}
+            alt={`Logo of ${clientSettings?.vendor?.name}`}
+            onError={(e) => {
+              e.target.style.display = "none";
+            }}
+          />
+        </a>
+      )}
+      <AccountContainer>
+        <UnauthenticatedTemplate>
+          <LoginButton onClick={login}>Log in</LoginButton>
+        </UnauthenticatedTemplate>
+        <AuthenticatedTemplate>
+          <AccountNameContainer>{activeAccount?.username}</AccountNameContainer>
+          <LoginButton onClick={logout}>Log out</LoginButton>
+        </AuthenticatedTemplate>
+      </AccountContainer>
+    </header>
+  );
+};
 
 export default Header;
