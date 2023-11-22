e70aa7ecf7b063028f3d9968fe4f3646d018fe1f
danjov <daniel.jovanovic@geowerkstatt.ch>
2023-11-22 11:12:48 +0100

    Add permissions to /app folder to non-root user

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,7 @@ set -e
 echo -n "Fix permissions for mounted volumes ..." && \
   chown -R abc:abc $Storage__UploadDirectory && \
   chown -R abc:abc /etc/ssl/private && \
+  chown -R abc:abc /app && \
   echo "done!"
 
 
