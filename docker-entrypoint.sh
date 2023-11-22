815f4a62c42e73939d2955698c45be31605bc4f0
danjov <daniel.jovanovic@geowerkstatt.ch>
2023-11-22 12:23:01 +0100

    Revert "Use https in Dockerfile"
    This reverts commit 22e23b47f5618171d1737cac2c0330d09da3fd56.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,6 @@ set -e
 # Change owner for our uploads folder
 echo -n "Fix permissions for mounted volumes ..." && \
   chown -R abc:abc $Storage__UploadDirectory && \
-  chown -R abc:abc /etc/ssl/private && \
   echo "done!"
 
 
