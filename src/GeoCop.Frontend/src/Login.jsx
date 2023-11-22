feb5faedfccb6f3221939155cca9958c28777246
tschumpr <roswita.tschuemperlin@geowerkstatt.ch>
2023-11-22 13:47:40 +0100

    Add navigation buttons for admin

--- src/GeoCop.Frontend/src/Login.jsx
+++ src/GeoCop.Frontend/src/Login.jsx
@@ -1,18 +1,30 @@
-import { AuthenticatedTemplate, UnauthenticatedTemplate, useMsal } from "@azure/msal-react";
-import { Button } from "react-bootstrap";
+import {
+  AuthenticatedTemplate,
+  UnauthenticatedTemplate,
+  useMsal,
+} from "@azure/msal-react";
+import { Button, Dropdown, DropdownButton } from "react-bootstrap";
 import styled from "styled-components";
 
 const AccountContainer = styled.div`
-  align-items: center;
   display: flex;
+  flex-direction: column;
+`;
+
+const LoggedInButtonGroup = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
 `;
 
 const AccountNameContainer = styled.span`
-  margin-right: 10px;
+  font-size: 14px;
 `;
 
 const LoginButton = styled(Button)`
   font-family: "Dosis", sans-serif;
+  width: 100px;
+  margin-left: 10px;
 `;
 
 export const Login = ({ clientSettings }) => {
@@ -44,8 +56,23 @@ export const Login = ({ clientSettings }) => {
         <LoginButton onClick={login}>Log in</LoginButton>
       </UnauthenticatedTemplate>
       <AuthenticatedTemplate>
-        <AccountNameContainer>{activeAccount?.username}</AccountNameContainer>
-        <LoginButton onClick={logout}>Log out</LoginButton>
+        <LoggedInButtonGroup>
+          <DropdownButton title="Administration">
+            <Dropdown.Item href="https://browser.geocop.ch">
+              Datenabgabe
+            </Dropdown.Item>
+            <Dropdown.Item href="https://browser.geocop.ch/admin">
+              Abgabeübersicht
+            </Dropdown.Item>
+            <Dropdown.Item href="https://browser.geocop.ch">
+              STAC Browser
+            </Dropdown.Item>
+          </DropdownButton>
+          <LoginButton onClick={logout}>Log out</LoginButton>
+        </LoggedInButtonGroup>
+        <AccountNameContainer>
+          Angemeldet als {activeAccount?.username}
+        </AccountNameContainer>
       </AuthenticatedTemplate>
     </AccountContainer>
   );
