476af11170a36a4f64db5fd9167f8c892bc273a4
tschumpr <roswita.tschuemperlin@geowerkstatt.ch>
2023-11-24 13:27:18 +0100

    Remove unnecessary authentication template

--- src/GeoCop.Frontend/src/pages/admin/Admin.jsx
+++ src/GeoCop.Frontend/src/pages/admin/Admin.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { AuthenticatedTemplate, UnauthenticatedTemplate, useMsal } from "@azure/msal-react";
+import { useMsal } from "@azure/msal-react";
 import { Button, Modal } from "react-bootstrap";
 import { GoTrash } from "react-icons/go";
 import { DataGrid, deDE } from "@mui/x-data-grid";
@@ -21,9 +21,11 @@ export const Admin = () => {
 
   if (activeAccount && deliveries.length == 0) {
     fetch("/api/v1/delivery")
-      .then((res) => res.headers.get("content-type")?.includes("application/json") && res.json())
+      .then((res) => res.ok && res.headers.get("content-type")?.includes("application/json") && res.json())
       .then((deliveries) => {
-        setDeliveries(deliveries);
+        if (deliveries) {
+          setDeliveries(deliveries);
+        }
       });
   }
 
@@ -43,72 +45,64 @@ export const Admin = () => {
   return (
     <>
       <main>
-        <UnauthenticatedTemplate>
-          <div className="admin-no-access">
-            <div className="app-subtitle">Bitte melden Sie sich an.</div>
+        {deliveries.length > 0 && (
+          <DataGrid
+            localeText={deDE.components.MuiDataGrid.defaultProps.localeText}
+            sx={{
+              margin: "20px 35px",
+              fontFamily: "system-ui, -apple-syste",
+            }}
+            pagination
+            rows={deliveries}
+            columns={columns}
+            initialState={{
+              pagination: {
+                paginationModel: { page: 0, pageSize: 10 },
+              },
+            }}
+            pageSizeOptions={[5, 10, 25]}
+            checkboxSelection
+            onRowSelectionModelChange={(newSelection) => {
+              setSelectedRows(newSelection);
+            }}
+            hideFooterRowCount
+            hideFooterSelectedRowCount
+          />
+        )}
+        {selectedRows.length > 0 && (
+          <div className="center-button-container">
+            <Button
+              className="icon-button"
+              onClick={() => {
+                setShowModal(true);
+              }}
+            >
+              <GoTrash />
+              <div style={{ marginLeft: 10 }}>
+                {selectedRows.length} Datenabgabe
+                {selectedRows.length > 1 ? "n" : ""} löschen
+              </div>
+            </Button>
           </div>
-        </UnauthenticatedTemplate>
-        <AuthenticatedTemplate>
-          <div className="app-title">Datenabgaben</div>
-          {deliveries.length > 0 && (
-            <DataGrid
-              localeText={deDE.components.MuiDataGrid.defaultProps.localeText}
-              sx={{
-                margin: "20px 35px",
-                fontFamily: "system-ui, -apple-syste",
+        )}
+        <Modal show={showModal} animation={false}>
+          <Modal.Body>
+            Möchten Sie die Datenabgabe wirklich löschen? Diese Aktion kann nicht rückgängig gemacht werden.
+          </Modal.Body>
+          <Modal.Footer>
+            <Button
+              variant="secondary"
+              onClick={() => {
+                setShowModal(false);
               }}
-              pagination
-              rows={deliveries}
-              columns={columns}
-              initialState={{
-                pagination: {
-                  paginationModel: { page: 0, pageSize: 10 },
-                },
-              }}
-              pageSizeOptions={[5, 10, 25]}
-              checkboxSelection
-              onRowSelectionModelChange={(newSelection) => {
-                setSelectedRows(newSelection);
-              }}
-              hideFooterRowCount
-              hideFooterSelectedRowCount
-            />
-          )}
-          {selectedRows.length > 0 && (
-            <div className="center-button-container">
-              <Button
-                className="icon-button"
-                onClick={() => {
-                  setShowModal(true);
-                }}
-              >
-                <GoTrash />
-                <div style={{ marginLeft: 10 }}>
-                  {selectedRows.length} Datenabgabe
-                  {selectedRows.length > 1 ? "n" : ""} löschen
-                </div>
-              </Button>
-            </div>
-          )}
-          <Modal show={showModal} animation={false}>
-            <Modal.Body>
-              Möchten Sie die Datenabgabe wirklich löschen? Diese Aktion kann nicht rückgängig gemacht werden.
-            </Modal.Body>
-            <Modal.Footer>
-              <Button
-                variant="secondary"
-                onClick={() => {
-                  setShowModal(false);
-                }}
-              >
-                Abbrechen
-              </Button>
-              <Button variant="danger" onClick={handleDelete}>
-                Löschen
-              </Button>
-            </Modal.Footer>
-          </Modal>
-        </AuthenticatedTemplate>
+            >
+              Abbrechen
+            </Button>
+            <Button variant="danger" onClick={handleDelete}>
+              Löschen
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </main>
     </>
   );
