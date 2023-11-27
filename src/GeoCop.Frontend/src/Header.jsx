47e0a00a1238f7f7dff7d1e46bf1aa46bc4487ca
tschumpr <roswita.tschuemperlin@geowerkstatt.ch>
2023-11-27 07:54:06 +0100

    Fix encoding

--- src/GeoCop.Frontend/src/Header.jsx
+++ src/GeoCop.Frontend/src/Header.jsx
@@ -1,4 +1,4 @@
-import { Button, Navbar, Nav, Container } from "react-bootstrap";
+﻿import { Button, Navbar, Nav, Container } from "react-bootstrap";
 import { AuthenticatedTemplate, UnauthenticatedTemplate, useMsal } from "@azure/msal-react";
 import { NavLink } from "react-router-dom";
 
@@ -32,17 +32,17 @@ export const Header = ({ clientSettings }) => {
       <Navbar expand="md" className="full-width justify-content-between" sticky="top">
         <Container fluid>
           {clientSettings?.vendor?.logo && (
-          <Navbar.Brand href={clientSettings?.vendor?.url} target="_blank" rel="noreferrer">
-                <img
-                  className="vendor-logo"
-                  src={clientSettings?.vendor?.logo}
-                  alt={`Logo of ${clientSettings?.vendor?.name}`}
-                  onError={(e) => {
-                    e.target.style.display = "none";
-                  }}
-                />
+            <Navbar.Brand href={clientSettings?.vendor?.url} target="_blank" rel="noreferrer">
+              <img
+                className="vendor-logo"
+                src={clientSettings?.vendor?.logo}
+                alt={`Logo of ${clientSettings?.vendor?.name}`}
+                onError={(e) => {
+                  e.target.style.display = "none";
+                }}
+              />
             </Navbar.Brand>
-            )}
+          )}
           <Navbar.Toggle aria-controls="navbar-nav" />
           <Navbar.Collapse id="navbar-nav">
             <div className="navbar-container">
