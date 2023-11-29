c0998d2bb871251db415e6081606478ce50f8283
tschumpr <roswita.tschuemperlin@geowerkstatt.ch>
2023-11-29 10:30:18 +0100

    Integrate DirectoryProvider

--- src/GeoCop.Api/FileAccess/PersistedAssetDeleter.cs
+++ src/GeoCop.Api/FileAccess/PersistedAssetDeleter.cs
@@ -1,4 +1,4 @@
-﻿namespace GeoCop.Api;
+﻿namespace GeoCop.Api.FileAccess;
 
 /// <summary>
 /// Migrates files delivered for validation into a persistent storage.
@@ -6,19 +6,18 @@
 public class PersistedAssetDeleter : IPersistedAssetDeleter
 {
     private readonly ILogger<PersistedAssetDeleter> logger;
-    private readonly string assetDirectory;
+    private readonly IDirectoryProvider directoryProvider;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="PersistedAssetDeleter"/> class.
     /// </summary>
     /// <param name="logger">The logger used for the instance.</param>
-    /// <param name="configuration">The service configuration.</param>
+    /// <param name="directoryProvider">The service configuration.</param>
     /// <exception cref="InvalidOperationException">Thrown if required configuration values are not defined.</exception>
-    public PersistedAssetDeleter(ILogger<PersistedAssetDeleter> logger, IConfiguration configuration)
+    public PersistedAssetDeleter(ILogger<PersistedAssetDeleter> logger, IDirectoryProvider directoryProvider)
     {
         this.logger = logger;
-        assetDirectory = configuration.GetValue<string>("Storage:AssetsDirectory")
-            ?? throw new InvalidOperationException("Missing root directory for persisted assets, the value can be configured as \"Storage:AssetsDirectory\"");
+        this.directoryProvider = directoryProvider;
     }
 
     /// <inheritdoc/>
@@ -26,7 +25,7 @@ public class PersistedAssetDeleter : IPersistedAssetDeleter
     {
         try
         {
-            Directory.Delete(Path.Combine(assetDirectory, jobId.ToString()), true);
+            Directory.Delete(directoryProvider.GetAssetDirectoryPath(jobId), true);
         }
         catch (Exception e)
         {
---
+++ tests/GeoCop.Api.Test/FileAccess/PersistedAssetDeleterTest.cs
@@ -0,0 +1,35 @@
+﻿using GeoCop.Api.Test;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace GeoCop.Api.FileAccess;
+
+[TestClass]
+public class PersistedAssetDeleterTest
+{
+    private Mock<ILogger<PersistedAssetDeleter>> loggerMock;
+    private PersistedAssetDeleter deleter;
+    private Guid jobId;
+    private string assetDirectory;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        jobId = Guid.NewGuid();
+        assetDirectory = Initialize.TestDirectoryProvider.GetAssetDirectoryPath(jobId);
+        loggerMock = new Mock<ILogger<PersistedAssetDeleter>>();
+        deleter = new PersistedAssetDeleter(loggerMock.Object, Initialize.TestDirectoryProvider);
+    }
+
+    [TestMethod]
+    public void DeleteJobAssets()
+    {
+        Directory.CreateDirectory(assetDirectory);
+        File.WriteAllText(Path.Combine(assetDirectory, "TempFileName"), "Some Content");
+
+        deleter.DeleteJobAssets(jobId);
+
+        Assert.IsFalse(Directory.Exists(assetDirectory));
+    }
+}
