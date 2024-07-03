69e5b1e28db70f7c43bf2fea790c01c60ff401a0
tschumpr <roswita.tschuemperlin@geowerkstatt.ch>
2024-07-03 09:14:10 +0200

    Add drawer to navigate between tools

--- src/Geopilot.Frontend/src/Header.jsx
+++ src/Geopilot.Frontend/src/Header.jsx
@@ -1,21 +1,46 @@
-﻿import { Button, Navbar, Nav, Container } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
-import { useAuth } from "./auth";
+﻿import { useAuth } from "./auth";
 import { useTranslation } from "react-i18next";
-import { AdminTemplate } from "./auth/AdminTemplate";
-import { LoggedInTemplate } from "./auth/LoggedInTemplate";
-import { LoggedOutTemplate } from "./auth/LoggedOutTemplate";
+import * as React from "react";
+import {
+  AppBar,
+  Box,
+  Button,
+  Divider,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { LoggedInTemplate } from "./auth/LoggedInTemplate.jsx";
+import { LoggedOutTemplate } from "./auth/LoggedOutTemplate.jsx";
+import { AdminTemplate } from "./auth/AdminTemplate.jsx";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export const Header = ({ clientSettings }) => {
   const { user, login, logout } = useAuth();
   const { t } = useTranslation();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [userMenuOpen, setUserMenuOpen] = React.useState(false);
+
+  const toggleUserMenu = newOpen => () => {
+    setUserMenuOpen(newOpen);
+  };
 
   return (
     <header>
-      <Navbar expand="md" className="full-width justify-content-between" sticky="top">
-        <Container fluid className="align-items-baseline">
-          {clientSettings?.vendor?.logo && (
-            <Navbar.Brand href={clientSettings?.vendor?.url} target="_blank" rel="noreferrer">
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+            }}>
+            <Box sx={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
               <img
                 className="vendor-logo"
                 src={clientSettings?.vendor?.logo}
@@ -24,45 +49,72 @@ export const Header = ({ clientSettings }) => {
                   e.target.style.display = "none";
                 }}
               />
-            </Navbar.Brand>
-          )}
-          <Navbar.Toggle aria-controls="navbar-nav" />
-          <Navbar.Collapse id="navbar-nav">
-            <div className="navbar-container">
-              <Nav className="full-width mr-auto" navbarScroll>
-                <NavLink className="nav-link" to="/">
-                  {t("delivery").toUpperCase()}
-                </NavLink>
-                <AdminTemplate>
-                  <NavLink className="nav-link" to="/admin">
-                    {t("deliveryOverview").toUpperCase()}
-                  </NavLink>
-                  <a className="nav-link" href="/browser">
-                    {t("stacBrowser").toUpperCase()}
-                  </a>
-                </AdminTemplate>
-              </Nav>
-              <Nav>
-                <LoggedOutTemplate>
-                  <Button className="nav-button" onClick={login}>
-                    {t("logIn")}
-                  </Button>
-                </LoggedOutTemplate>
-                <LoggedInTemplate>
-                  <Button className="nav-button" onClick={logout}>
-                    {t("logOut")}
-                  </Button>
-                </LoggedInTemplate>
-              </Nav>
-            </div>
-            <div className="navbar-info-container">
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                {location.pathname.includes("admin") ? t("administration").toUpperCase() : t("delivery").toUpperCase()}
+              </Typography>
+            </Box>
+            <Box sx={{ flexGrow: 0 }}>
+              <LoggedOutTemplate>
+                <Button color="inherit" onClick={login}>
+                  {t("logIn")}
+                </Button>
+              </LoggedOutTemplate>
               <LoggedInTemplate>
-                <div className="user-info">{t("loggedInAs", { name: user?.name })}</div>
+                <Button className="nav-button" sx={{ color: "white" }} onClick={toggleUserMenu(true)}>
+                  {user?.name}
+                </Button>
               </LoggedInTemplate>
-            </div>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+            </Box>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Drawer anchor={"right"} open={userMenuOpen} onClose={toggleUserMenu(false)}>
+        <div className="user-menu">
+          <Box
+            sx={{ width: 250 }}
+            role="presentation"
+            onClick={toggleUserMenu(false)}
+            onKeyDown={toggleUserMenu(false)}>
+            <List>
+              <ListItem key={user?.name}>
+                <ListItemText primary={user?.name} />
+              </ListItem>
+            </List>
+            <Divider />
+            <List>
+              <ListItem key={t("delivery").toUpperCase()} disablePadding>
+                <ListItemButton
+                  onClick={() => {
+                    navigate("/");
+                  }}>
+                  <ListItemText primary={t("delivery").toUpperCase()} />
+                </ListItemButton>
+              </ListItem>
+              <AdminTemplate>
+                <ListItem key={t("administration").toUpperCase()} disablePadding>
+                  <ListItemButton
+                    onClick={() => {
+                      navigate("/admin");
+                    }}>
+                    <ListItemText primary={t("administration").toUpperCase()} />
+                  </ListItemButton>
+                </ListItem>
+                <ListItem key={t("stacBrowser").toUpperCase()} disablePadding>
+                  <ListItemButton
+                    onClick={() => {
+                      window.location.href = "/browser";
+                    }}>
+                    <ListItemText primary={t("stacBrowser").toUpperCase()} />
+                  </ListItemButton>
+                </ListItem>
+              </AdminTemplate>
+            </List>
+          </Box>
+          <Button className="nav-button" sx={{ color: "black" }} onClick={logout}>
+            {t("logOut")}
+          </Button>
+        </div>
+      </Drawer>
     </header>
   );
 };
