a21dd797d6d8960f419301f8fa17a224a0edb517
Philipp Lüthi <philipp.luethi@geowerkstatt.ch>
2024-07-12 17:53:35 +0200

    Remove client specific api parameters

--- src/Geopilot.Api/Contracts/BrowserAuthOptions.cs
+++ src/Geopilot.Api/Contracts/BrowserAuthOptions.cs
@@ -14,19 +14,4 @@ public class BrowserAuthOptions
     /// The client id of the application registered at the authority.
     /// </summary>
     public string ClientId { get; set; } = string.Empty;
-
-    /// <summary>
-    /// OAuth redirect URI.
-    /// </summary>
-    public string? RedirectUri { get; set; }
-
-    /// <summary>
-    /// Redirect URI after logout.
-    /// </summary>
-    public string? PostLogoutRedirectUri { get; set; }
-
-    /// <summary>
-    /// If true, navigate to the original request URL after login.
-    /// </summary>
-    public bool? NavigateToLoginRequestUrl { get; set; }
 }
