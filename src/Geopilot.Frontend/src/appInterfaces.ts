bb0449d0efa93c3dc9fef6d56fa85b19388c8535
tschumpr <roswita.tschuemperlin@geowerkstatt.ch>
2024-08-22 08:36:18 +0200

    Remove unused code

--- src/Geopilot.Frontend/src/appInterfaces.ts
+++ src/Geopilot.Frontend/src/appInterfaces.ts
@@ -9,13 +9,6 @@ export interface TranslationFunction {
   (key: string): string;
 }
 
-export type ModalContentType = "markdown" | "raw";
-
 export interface Validation {
   allowedFileExtensions: string[];
-}
-
-export interface ErrorResponse {
-  status: string;
-  detail: string;
 }
