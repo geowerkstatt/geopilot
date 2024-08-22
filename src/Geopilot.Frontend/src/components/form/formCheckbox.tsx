ed4def3ff5e62f5c3c805b06c1c1cfa348e1f086
tschumpr <roswita.tschuemperlin@geowerkstatt.ch>
2024-08-22 08:42:45 +0200

    Add form components

---
+++ src/Geopilot.Frontend/src/components/form/formCheckbox.tsx
@@ -0,0 +1,33 @@
+import { Checkbox, FormControlLabel, SxProps } from "@mui/material";
+import { useTranslation } from "react-i18next";
+import { useFormContext } from "react-hook-form";
+import { FC, ReactNode } from "react";
+
+export interface FormCheckboxProps {
+  fieldName: string;
+  label: string | ReactNode;
+  checked: boolean;
+  disabled?: boolean;
+  sx?: SxProps;
+  validation?: object;
+}
+
+export const FormCheckbox: FC<FormCheckboxProps> = ({ fieldName, label, checked, disabled, sx, validation }) => {
+  const { t } = useTranslation();
+  const { register } = useFormContext();
+
+  return (
+    <FormControlLabel
+      sx={{ marginTop: "10px!important", marginRight: "10px!important", ...sx }}
+      control={
+        <Checkbox
+          data-cy={fieldName + "-formCheckbox"}
+          {...register(fieldName, validation)}
+          disabled={disabled || false}
+          defaultChecked={checked || false}
+        />
+      }
+      label={typeof label === "string" ? t(label) : label}
+    />
+  );
+};
