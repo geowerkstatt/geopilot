0a411a54d9032378d3a8255a408fe9f1b7a3a33e
tschumpr <roswita.tschuemperlin@geowerkstatt.ch>
2025-01-31 15:08:30 +0100

    Add user detail page

--- src/Geopilot.Frontend/src/pages/admin/users.tsx
+++ src/Geopilot.Frontend/src/pages/admin/users.tsx
@@ -1,92 +1,42 @@
 import { useTranslation } from "react-i18next";
-import { CircularProgress, Stack } from "@mui/material";
-import { AdminGrid } from "../../components/adminGrid/adminGrid";
+import { Tooltip } from "@mui/material";
 import { DataRow, GridColDef } from "../../components/adminGrid/adminGridInterfaces";
 import { Organisation, User } from "../../api/apiInterfaces";
-import { useCallback, useContext, useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useGeopilotAuth } from "../../auth";
-import { PromptContext } from "../../components/prompt/promptContext";
 import { useApi } from "../../api";
-import CancelOutlinedIcon from "@mui/icons-material/CancelOutlined";
-import LinkOffIcon from "@mui/icons-material/LinkOff";
+import { DataGrid, GridActionsCellItem, GridRowId } from "@mui/x-data-grid";
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
+import { useControlledNavigate } from "../../components/controlledNavigate";
 
 export const Users = () => {
   const { t } = useTranslation();
   const { user } = useGeopilotAuth();
+  const { navigateTo } = useControlledNavigate();
   const [users, setUsers] = useState<User[]>();
-  const [organisations, setOrganisations] = useState<Organisation[]>();
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const { showPrompt } = useContext(PromptContext);
   const { fetchApi } = useApi();
-
-  useEffect(() => {
-    if (users && organisations) {
-      setIsLoading(false);
-    }
-  }, [users, organisations]);
 
   const loadUsers = useCallback(() => {
     fetchApi<User[]>("/api/v1/user", { errorMessageLabel: "usersLoadingError" }).then(setUsers);
   }, [fetchApi]);
 
-  const loadOrganisations = useCallback(() => {
-    fetchApi<Organisation[]>("/api/v1/organisation", { errorMessageLabel: "organisationsLoadingError" }).then(
-      setOrganisations,
-    );
-  }, [fetchApi]);
-
-  async function saveUser(user: User) {
-    user.organisations = user.organisations?.map(value =>
-      typeof value === "number"
-        ? ({ id: value } as Organisation)
-        : ({ id: (value as Organisation).id } as Organisation),
-    );
-    delete user.deliveries;
-    fetchApi("/api/v1/user", {
-      method: "PUT",
-      body: JSON.stringify(user),
-      errorMessageLabel: "userSaveError",
-    }).then(loadUsers);
-  }
-
-  async function onSave(row: DataRow) {
-    await saveUser(row as User);
-  }
-
-  async function onDisconnect(row: DataRow) {
-    showPrompt("userDisconnect", [
-      { label: "cancel", icon: <CancelOutlinedIcon /> },
-      {
-        label: "disconnect",
-        icon: <LinkOffIcon />,
-        action: () => {
-          const user = row as unknown as User;
-          user.organisations = [];
-          saveUser(user);
-        },
-        color: "error",
-        variant: "contained",
-      },
-    ]);
-  }
+  const startEditing = (id: GridRowId) => {
+    navigateTo(`/admin/users/${id}`);
+  };
 
   useEffect(() => {
     if (user?.isAdmin) {
       if (users === undefined) {
         loadUsers();
       }
-      if (organisations === undefined) {
-        loadOrganisations();
-      }
     }
-  }, [loadOrganisations, loadUsers, organisations, user?.isAdmin, users]);
+  }, [loadUsers, users, user?.isAdmin]);
 
   const columns: GridColDef[] = [
     {
       field: "fullName",
       headerName: t("name"),
       type: "string",
-      editable: false,
       flex: 1,
       minWidth: 200,
     },
@@ -94,42 +44,63 @@ export const Users = () => {
       field: "email",
       headerName: t("email"),
       type: "string",
-      editable: false,
       flex: 1,
       minWidth: 280,
     },
     {
       field: "isAdmin",
       headerName: t("isAdmin"),
-      editable: true,
       width: 160,
       type: "boolean",
     },
     {
       field: "organisations",
       headerName: t("organisations"),
-      editable: true,
       flex: 1,
       minWidth: 400,
-      type: "custom",
-      valueOptions: organisations,
-      getOptionLabel: (value: DataRow | string) => (value as Organisation).name,
-      getOptionValue: (value: DataRow | string) => (value as Organisation).id,
+      valueFormatter: (organisations: Organisation[]) => {
+        return organisations?.map(o => o.name).join(", ");
+      },
+    },
+    {
+      field: "actions",
+      type: "actions",
+      headerName: "",
+      flex: 0,
+      resizable: false,
+      cellClassName: "actions",
+      getActions: ({ id }) => [
+        <Tooltip title={t("edit")} key={`edit-${id}`}>
+          <GridActionsCellItem
+            icon={<EditOutlinedIcon />}
+            label={t("edit")}
+            onClick={() => startEditing(id)}
+            color="inherit"
+          />
+        </Tooltip>,
+      ],
     },
   ];
 
-  return isLoading ? (
-    <Stack sx={{ flex: "1 0 0", justifyContent: "center", alignItems: "center", height: "100%" }}>
-      <CircularProgress />
-    </Stack>
-  ) : (
-    <AdminGrid
-      data={users as unknown as DataRow[]}
-      columns={columns}
-      onSave={onSave}
-      onDisconnect={onDisconnect}
-      disableRow={user?.id}
-    />
+  return (
+    <>
+      <DataGrid
+        data-cy="users-grid"
+        loading={!users?.length}
+        rows={users as unknown as DataRow[]}
+        columns={columns}
+        disableColumnSelector
+        hideFooterSelectedRowCount
+        pagination
+        pageSizeOptions={[5, 10, 25]}
+        initialState={{
+          pagination: { paginationModel: { pageSize: 10 } },
+        }}
+        onRowSelectionModelChange={newRowSelectionModel => {
+          startEditing(newRowSelectionModel[0]);
+        }}
+      />
+    </>
   );
 };
 
