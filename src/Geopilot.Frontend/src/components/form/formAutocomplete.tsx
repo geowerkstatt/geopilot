7a950cdf1f4df2bc1b0ebfb575df538227cf236d
Nenad Stevic <nenad.stevic@geowerkstatt.ch>
2025-03-11 09:30:59 +0100

    Move helper method to seperate file to satisfy ESLint

--- src/Geopilot.Frontend/src/components/form/formAutocomplete.tsx
+++ src/Geopilot.Frontend/src/components/form/formAutocomplete.tsx
@@ -35,20 +35,6 @@ export const FormAutocomplete: FC<FormAutocompleteProps> = ({
   const { t } = useTranslation();
   const { control, setValue } = useFormContext();
 
-  export const mapToFormAutocompleteValue = <T extends { id: number }>(
-    items: T[] | undefined,
-    getDisplayText: (item: T) => string,
-    getFullDisplayText: (item: T) => string,
-  ): FormAutocompleteValue[] => {
-    return (
-      items?.map(item => ({
-        id: item.id,
-        displayText: getDisplayText(item),
-        fullDisplayText: getFullDisplayText(item),
-      })) || []
-    );
-  };
-
   return (
     <Controller
       name={fieldName}
