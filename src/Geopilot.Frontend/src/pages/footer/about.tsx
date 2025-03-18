fb518bea480873211fa6966ecac1ff858058b2cb
Nenad Stevic <nenad.stevic@geowerkstatt.ch>
2025-03-18 20:14:41 +0100

    Remove all trace of licenses from about

--- src/Geopilot.Frontend/src/pages/footer/about.tsx
+++ src/Geopilot.Frontend/src/pages/footer/about.tsx
@@ -3,35 +3,13 @@ import { useEffect, useState } from "react";
 import { useApi } from "../../api";
 import { Link, Typography } from "@mui/material";
 import { MarkdownContent } from "../../components/markdownContent.tsx";
-import { ContentType } from "../../api/apiInterfaces.ts";
 import { CenteredBox } from "../../components/styledComponents.ts";
 import { useLocation } from "react-router-dom";
 import { useAppSettings } from "../../components/appSettings/appSettingsInterface.ts";
 
-interface PackageList {
-  [packageName: string]: PackageDetails;
-}
-
-interface PackageDetails {
-  licenses?: string;
-  repository?: string;
-  publisher?: string;
-  email?: string;
-  url?: string;
-  name: string;
-  version: string;
-  description?: string;
-  copyright?: string;
-  licenseText?: string;
-  path?: string;
-  licenseFile?: string;
-}
-
 export const About = () => {
   const { t, i18n } = useTranslation();
   const [info, setInfo] = useState<string>();
-  const [licenseInfo, setLicenseInfo] = useState<PackageList>();
-  const [licenseInfoCustom, setLicenseInfoCustom] = useState<PackageList>();
   const [version, setVersion] = useState<string | null>();
   const { termsOfUse } = useAppSettings();
   const { fetchApi, fetchLocalizedMarkdown } = useApi();
@@ -39,8 +17,6 @@ export const About = () => {
 
   useEffect(() => {
     fetchLocalizedMarkdown("info", i18n.language).then(setInfo);
-    fetchApi<PackageList>("/license.json", { responseType: ContentType.Json }).then(setLicenseInfo);
-    fetchApi<PackageList>("/license.custom.json", { responseType: ContentType.Json }).then(setLicenseInfoCustom);
     fetchApi<string>("/api/v1/version").then(version => {
       setVersion(version.split("+")[0]);
     });
@@ -59,7 +35,7 @@ export const About = () => {
     setTimeout(scrollToHash, 0);
 
     scrollToHash();
-  }, [hash, info, termsOfUse, licenseInfo, licenseInfoCustom, version]);
+  }, [hash, info, termsOfUse, version]);
 
   return (
     <CenteredBox>
@@ -98,47 +74,17 @@ export const About = () => {
           }}
         />
       </p>
-      {(licenseInfo || licenseInfoCustom) && (
-        <Typography variant="h1" id="licenses">
-          {t("licenseInformation")}
-        </Typography>
-      )}
-      {licenseInfoCustom &&
-        Object.keys(licenseInfoCustom).map(key => (
-          <div key={key} className="about-licenses">
-            <Typography variant="h3">
-              {licenseInfoCustom[key].name}
-              {licenseInfoCustom[key].version && ` (${t("version")} ${licenseInfoCustom[key].version})`}{" "}
-            </Typography>
-            <p>
-              <Link href={licenseInfoCustom[key].repository}>{licenseInfoCustom[key].repository}</Link>
-            </p>
-            <p>{licenseInfoCustom[key].description}</p>
-            <p>{licenseInfoCustom[key].copyright}</p>
-            <p>
-              {t("licenses")}: {licenseInfoCustom[key].licenses}
-            </p>
-            <p>{licenseInfoCustom[key].licenseText}</p>
-          </div>
-        ))}
-      {licenseInfo &&
-        Object.keys(licenseInfo).map(key => (
-          <div key={key} className="about-licenses">
-            <Typography variant="h3">
-              {licenseInfo[key].name}
-              {licenseInfo[key].version && ` (${t("version")} ${licenseInfo[key].version})`}{" "}
-            </Typography>
-            <p>
-              <Link href={licenseInfo[key].repository}>{licenseInfo[key].repository}</Link>
-            </p>
-            <p>{licenseInfo[key].description}</p>
-            <p>{licenseInfo[key].copyright}</p>
-            <p>
-              {t("licenses")}: {licenseInfo[key].licenses}
-            </p>
-            <p>{licenseInfo[key].licenseText}</p>
-          </div>
-        ))}
+      <Typography variant="h1" id="licenses">
+        {t("licenseInformation")}
+      </Typography>
+      <Typography variant="body1" id="licenses-text">
+        <Trans
+          i18nKey="licenseInformationDescription"
+          components={{
+            licenseLink: <Link href="/licenses" target="_self" rel="noreferrer" />,
+          }}
+        />
+      </Typography>
     </CenteredBox>
   );
 };
