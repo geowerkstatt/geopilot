40de8b4a1e397071ef178449835c695bd6d68065
Nenad Stevic <nenad.stevic@geowerkstatt.ch>
2025-03-24 12:31:18 +0100

    Wrap API helper methods in useCallback

--- src/Geopilot.Frontend/src/api/apiContext.tsx
+++ src/Geopilot.Frontend/src/api/apiContext.tsx
@@ -1,5 +1,5 @@
 import { ApiContextInterface, ApiError, ContentType, FetchParams } from "./apiInterfaces.ts";
-import { createContext, FC, PropsWithChildren, useContext } from "react";
+import { createContext, FC, PropsWithChildren, useCallback, useContext } from "react";
 import { AlertContext } from "../components/alert/alertContext.tsx";
 import { useTranslation } from "react-i18next";
 
@@ -16,57 +16,62 @@ export const ApiProvider: FC<PropsWithChildren> = ({ children }) => {
   const { t } = useTranslation();
   const { showAlert } = useContext(AlertContext);
 
-  async function fetchApi<T>(url: string, options: FetchParams = {}): Promise<T> {
-    try {
-      const response = await fetch(url, options);
-
-      if (response.ok) {
-        const responseContentType = response.headers.get("content-type");
-        if (responseContentType !== null && responseContentType?.indexOf("application/json") !== -1) {
-          return (await response.json()) as T;
-        } else if (!options.responseType || responseContentType?.includes(options.responseType)) {
-          return (await response.text()) as T;
-        } else {
-          throw new ApiError(t("invalidContentType", { contentType: responseContentType }));
-        }
-      } else {
-        let errorResponse;
-        const clonedResponse = response.clone();
-        try {
-          const errorObject = await clonedResponse.json();
-          if (errorObject.detail) {
-            errorResponse = errorObject.detail;
+  const fetchApi = useCallback(
+    async <T,>(url: string, options: FetchParams = {}): Promise<T> => {
+      try {
+        const response = await fetch(url, options);
+        if (response.ok) {
+          const responseContentType = response.headers.get("content-type");
+          if (responseContentType !== null && responseContentType?.indexOf("application/json") !== -1) {
+            return (await response.json()) as T;
+          } else if (!options.responseType || responseContentType?.includes(options.responseType)) {
+            return (await response.text()) as T;
           } else {
-            errorResponse = errorObject.title;
+            throw new ApiError(t("invalidContentType", { contentType: responseContentType }));
           }
-        } catch (e) {
-          errorResponse = await response.text();
-        }
-        throw new ApiError(errorResponse, response.status);
-      }
-    } catch (error) {
-      if (options.errorMessageLabel) {
-        showAlert(t(options.errorMessageLabel, { error: (error as Error)?.message }), "error");
-      }
-      if (error instanceof ApiError) {
-        throw error;
-      } else {
-        throw new ApiError(String(error));
-      }
-    }
-  }
-
-  const fetchLocalizedMarkdown = (markdown: string, language: string): Promise<string> => {
-    return fetchApi<string>(`/${markdown}.${language}.md`, { responseType: ContentType.Markdown })
-      .then(response => {
-        if (response) {
-          return response;
         } else {
-          throw new Error("Language-specific markdown not found");
+          let errorResponse;
+          const clonedResponse = response.clone();
+          try {
+            const errorObject = await clonedResponse.json();
+            if (errorObject.detail) {
+              errorResponse = errorObject.detail;
+            } else {
+              errorResponse = errorObject.title;
+            }
+          } catch (e) {
+            errorResponse = await response.text();
+          }
+          throw new ApiError(errorResponse, response.status);
         }
-      })
-      .catch(() => fetchApi<string>(`/${markdown}.md`, { responseType: ContentType.Markdown }));
-  };
+      } catch (error) {
+        if (options.errorMessageLabel) {
+          showAlert(t(options.errorMessageLabel, { error: (error as Error)?.message }), "error");
+        }
+        if (error instanceof ApiError) {
+          throw error;
+        } else {
+          throw new ApiError(String(error));
+        }
+      }
+    },
+    [showAlert, t],
+  );
+
+  const fetchLocalizedMarkdown = useCallback(
+    (markdown: string, language: string): Promise<string> => {
+      return fetchApi<string>(`/${markdown}.${language}.md`, { responseType: ContentType.Markdown })
+        .then(response => {
+          if (response) {
+            return response;
+          } else {
+            throw new Error("Language-specific markdown not found");
+          }
+        })
+        .catch(() => fetchApi<string>(`/${markdown}.md`, { responseType: ContentType.Markdown }));
+    },
+    [fetchApi],
+  );
 
   return <ApiContext.Provider value={{ fetchApi, fetchLocalizedMarkdown }}>{children}</ApiContext.Provider>;
 };
