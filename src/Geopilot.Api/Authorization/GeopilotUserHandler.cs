fb6ddd4622d515c7eb05daff30da630ea648ad85
Nenad Stevic <nenad.stevic@geowerkstatt.ch>
2025-06-24 10:11:03 +0200

    Fix warnings

--- src/Geopilot.Api/Authorization/GeopilotUserHandler.cs
+++ src/Geopilot.Api/Authorization/GeopilotUserHandler.cs
@@ -58,7 +58,7 @@ public class GeopilotUserHandler : AuthorizationHandler<GeopilotUserRequirement>
             return null;
         }
 
-        var user = await dbContext.Users.FirstOrDefaultAsync(u => u.Email.ToLower() == email);
+        var user = await dbContext.Users.FirstOrDefaultAsync(u => string.Equals(u.Email, email, StringComparison.OrdinalIgnoreCase));
         if (user == null)
         {
             user = new User { AuthIdentifier = sub, Email = email.ToLowerInvariant(), FullName = name };
