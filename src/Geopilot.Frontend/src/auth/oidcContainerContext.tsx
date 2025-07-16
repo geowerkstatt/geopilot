b7cf596a60d354566bd48799cb36f2d6570cc7d5
Nenad Stevic <nenad.stevic@geowerkstatt.ch>
2025-07-16 11:25:21 +0200

    Clean up context

--- src/Geopilot.Frontend/src/auth/oidcContainerContext.tsx
+++ src/Geopilot.Frontend/src/auth/oidcContainerContext.tsx
@@ -1,8 +1,7 @@
-import { FC, PropsWithChildren, useMemo } from "react";
+import { FC, PropsWithChildren } from "react";
 import { User, WebStorageStateStore } from "oidc-client-ts";
 import { useApiAuthConfiguration } from ".";
 import { AuthProvider, AuthProviderProps } from "react-oidc-context";
-import { useAppSettings } from "../components/appSettings/appSettingsInterface";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const onSigninCallback = (user: User | void) => {
@@ -10,26 +9,14 @@ const onSigninCallback = (user: User | void) => {
 };
 
 export const OidcContainerProvider: FC<PropsWithChildren> = ({ children }) => {
-  const { clientSettings } = useAppSettings();
   const apiSetting = useApiAuthConfiguration();
 
-  const scopes = useMemo(() => {
-    if (!clientSettings?.authScopes) return "";
-
-    const scopeList = [...clientSettings.authScopes];
-    if (apiSetting?.organizationId) {
-      scopeList.push(`urn:zitadel:iam:org:id:${apiSetting.organizationId}`);
-    }
-    return scopeList.join(" ");
-  }, [clientSettings?.authScopes, apiSetting?.organizationId]);
-
-  if (!clientSettings?.authScopes) return children;
   if (!apiSetting) return children;
 
   const oidcConfig: AuthProviderProps = {
     authority: apiSetting.authority,
     client_id: apiSetting.clientId,
-    scope: scopes,
+    scope: apiSetting.scope,
     redirect_uri: window.location.origin,
     post_logout_redirect_uri: window.location.origin,
     onSigninCallback: onSigninCallback,
