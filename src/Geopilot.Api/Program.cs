73411b8fe8d9bd06c1adcb66d56078edc123153b
Nenad Stevic <nenad.stevic@geowerkstatt.ch>
2025-09-24 12:27:24 +0200

    Use Api Client Id (conforms more to OAUTH2.0 Spec when working with resource servers)

--- src/Geopilot.Api/Program.cs
+++ src/Geopilot.Api/Program.cs
@@ -56,7 +56,7 @@ builder.Services
     .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
     {
         options.Authority = builder.Configuration["Auth:Authority"];
-        options.Audience = builder.Configuration["Auth:ClientId"];
+        options.Audience = builder.Configuration["Auth:ApiId"];
         options.RequireHttpsMetadata = !builder.Environment.IsDevelopment();
         options.MapInboundClaims = false;
 
