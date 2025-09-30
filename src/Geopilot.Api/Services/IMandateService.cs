3a4d583693d6b50dce1c89cf9abc958c44142869
Philipp Lüthi <philipp.luethi@geowerkstatt.ch>
2025-09-30 10:42:40 +0200

    Add define empty returns or remove on Controllers

--- src/Geopilot.Api/Services/IMandateService.cs
+++ src/Geopilot.Api/Services/IMandateService.cs
@@ -12,6 +12,7 @@ public interface IMandateService
     /// </summary>
     /// <param name="user">If provided, only mandates this user can make deliveries for are returned.</param>
     /// <param name="jobId">If provided, only mandates that accept the file type of the file of the job are returned.</param>
+    /// <returns>List of <see cref="Mandate"/> deliverable by user and job.</returns>
     Task<List<Mandate>> GetMandatesAsync(User? user = null, Guid? jobId = null);
 
     /// <summary>
@@ -21,12 +22,12 @@ public interface IMandateService
     /// <param name="mandateId">The id of the mandate to retrieve.</param>
     /// <param name="user">The user that tries to access the mandate.</param>
     /// <param name="jobId">The job for which the mandate should be retrieved.</param>
-    /// <returns></returns>
+    /// <returns>The <see cref="Mandate"/> if found and accessible; otherwise, null.</returns>
     Task<Mandate?> GetMandateByUserAndJobAsync(int mandateId, User user, Guid jobId);
 
     /// <summary>
     /// Retrieves a list of all file extensions that are accepted by any mandate in the system.
     /// </summary>
-    /// <returns></returns>
+    /// <returns>A set of accepted file extensions defined by the mandates.</returns>
     HashSet<string> GetFileExtensionsForMandates();
 }
