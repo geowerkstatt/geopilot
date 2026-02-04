585e8983944e91ecc59d7c41655d38efb3679efa
Lukas Oldani <lukas.oldani@geowerkstatt.ch>
2026-02-04 08:57:42 +0100

    chore: extract pipeline integration test to its deticated integration test
    Issue-Id: #526

--- tests/Geopilot.Api.Test/Pipeline/PipelineTest.cs
+++ tests/Geopilot.Api.Test/Pipeline/PipelineTest.cs
@@ -1,59 +1,12 @@
-﻿using Geopilot.Api.FileAccess;
-using Geopilot.Api.Pipeline;
+﻿using Geopilot.Api.Pipeline;
 using Geopilot.Api.Pipeline.Config;
 using Moq;
-using System.Reflection;
 
 namespace Geopilot.Api.Test.Pipeline;
 
 [TestClass]
 public class PipelineTest
 {
-    [TestMethod]
-    public void RunTwoStepPipeline()
-    {
-        var uploadStepId = "upload";
-        var validationStepId = "validation";
-        var dummyStepId = "dummy";
-        var uploadedFileAttribute = "ili_file";
-
-        PipelineFactory factory = CreatePipelineFactory("twoStepPipeline_01");
-        var pipeline = factory.CreatePipeline("two_steps");
-        Assert.IsNotNull(pipeline, "pipeline not created");
-        Assert.HasCount(2, pipeline.Steps);
-
-        using var fileHandle = CreateTestFileHandle("TestData/UploadFiles/RoadsExdm2ien.xtf");
-        var context = pipeline.Run(fileHandle);
-
-        Assert.AreEqual(PipelineState.Success, pipeline.State);
-        Assert.AreEqual(StepState.Success, pipeline.Steps[0].State);
-        Assert.AreEqual(StepState.Success, pipeline.Steps[1].State);
-
-        // Assert if uploaded file was correctly added to PipelineContext
-        var stepResults = context.StepResults;
-
-        Assert.IsTrue(stepResults.ContainsKey(uploadStepId));
-        var uploadStepResult = context.StepResults[uploadStepId];
-        Assert.HasCount(1, uploadStepResult.Outputs, "upload step has not the expected number of data");
-        Assert.IsTrue(uploadStepResult.Outputs.ContainsKey(uploadedFileAttribute));
-        var uploadedFileStepOutput = uploadStepResult.Outputs[uploadedFileAttribute];
-
-        Assert.IsNotNull(uploadedFileStepOutput.Data);
-        var uploadedFile = uploadedFileStepOutput.Data as FileHandle;
-        Assert.IsNotNull(uploadedFile);
-        Assert.AreEqual(fileHandle.FileName, uploadedFile.FileName);
-
-        // Assert if StepResults from executed PipelineSteps are in the PipelineContext
-        Assert.HasCount(3, stepResults);
-        Assert.IsTrue(stepResults.ContainsKey(validationStepId));
-        var validationSetpResult = stepResults[validationStepId];
-        Assert.HasCount(2, validationSetpResult.Outputs, "validation step has not the expected number of data");
-
-        Assert.IsTrue(stepResults.ContainsKey(dummyStepId));
-        var dummyStepResult = stepResults[dummyStepId];
-        Assert.HasCount(1, dummyStepResult.Outputs, "dummy step has not the expected number of data");
-    }
-
     [TestMethod(DisplayName = "Pipeline State Test")]
     [DataRow(PipelineState.Pending, new[] { StepState.Pending, StepState.Pending })]
     [DataRow(PipelineState.Running, new[] { StepState.Running, StepState.Pending })]
@@ -79,18 +32,5 @@ public class PipelineTest
         var pipeline = new Api.Pipeline.Pipeline("test_pipeline", pipelineDisplayName, steps, pipelineParameters);
 
         Assert.AreEqual(expectedState, pipeline.State);
-    }
-
-    private PipelineFactory CreatePipelineFactory(string filename)
-    {
-        string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"TestData/Pipeline/" + filename + ".yaml");
-        return PipelineFactory.FromFile(path);
-    }
-
-    private FileHandle CreateTestFileHandle(string file)
-    {
-        var tempFilePath = Path.GetTempFileName();
-        var stream = File.Open(file, FileMode.Open, System.IO.FileAccess.Read);
-        return new FileHandle(file, stream);
     }
 }
