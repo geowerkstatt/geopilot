94e74bf6351d8a5e4adcfb4806ed6851587a7eeb
Joel Wälti <joel.waelti@geowerkstatt.ch>
2026-02-26 11:14:51 +0100

    chore: add tests for wrong input types when running steps

--- tests/Geopilot.Api.Test/Pipeline/PipelineStepTest.cs
+++ tests/Geopilot.Api.Test/Pipeline/PipelineStepTest.cs
@@ -59,14 +59,14 @@ public class PipelineStepTest
 
         private Dictionary<string, object> outputData;
 
-        public int NumberOfRunInvoced { get; set; }
+        public int NumberOfRunInvokations { get; set; }
 
         [PipelineProcessRun]
-        public async Task<Dictionary<string, object>> RunAsync(string[] srtingData, int intData, bool booleanData, double doubleData, CancellationToken cancellationToken)
+        public async Task<Dictionary<string, object>> RunAsync(string[] stringData, int intData, bool booleanData, double doubleData, CancellationToken cancellationToken)
         {
-            NumberOfRunInvoced++;
-            Assert.IsNotNull(srtingData);
-            Assert.IsNotEmpty(srtingData, "data expected");
+            NumberOfRunInvokations++;
+            Assert.IsNotNull(stringData);
+            Assert.IsNotEmpty(stringData, "data expected");
             Assert.IsNotNull(intData);
             Assert.IsNotNull(booleanData);
             Assert.IsNotNull(doubleData);
@@ -248,30 +248,10 @@ public class PipelineStepTest
     {
         var inputConfigs = new List<InputConfig>
         {
-            new InputConfig
-            {
-                From = "step_01",
-                Take = "srtingData",
-                As = "srtingData",
-            },
-            new InputConfig
-            {
-                From = "step_01",
-                Take = "intData",
-                As = "intData",
-            },
-            new InputConfig
-            {
-                From = "step_01",
-                Take = "booleanData",
-                As = "booleanData",
-            },
-            new InputConfig
-            {
-                From = "step_01",
-                Take = "doubleData",
-                As = "doubleData",
-            },
+            NewInputConfig("step_01", "stringData", "stringData"),
+            NewInputConfig("step_01", "intData", "intData"),
+            NewInputConfig("step_01", "booleanData", "booleanData"),
+            NewInputConfig("step_01", "doubleData", "doubleData"),
         };
         var outputConfigs = new List<OutputConfig>
         {
@@ -286,7 +266,7 @@ public class PipelineStepTest
         {
             Outputs = new Dictionary<string, StepOutput>
             {
-                { "srtingData", new StepOutput { Action = new HashSet<OutputAction>(), Data = "some data from step 01" } },
+                { "stringData", new StepOutput { Action = new HashSet<OutputAction>(), Data = "some data from step 01" } },
                 { "intData", new StepOutput { Action = new HashSet<OutputAction>(), Data = 123456 } },
                 { "booleanData", new StepOutput { Action = new HashSet<OutputAction>(), Data = false } },
                 { "doubleData", new StepOutput { Action = new HashSet<OutputAction>(), Data = 123.456 } },
@@ -316,7 +296,7 @@ public class PipelineStepTest
 
         Assert.AreEqual(StepState.Success, pipelineStep.State);
 
-        Assert.AreEqual(1, processMock.NumberOfRunInvoced, "Process Run method was not invoked exactly once.");
+        Assert.AreEqual(1, processMock.NumberOfRunInvokations, "Process Run method was not invoked exactly once.");
 
         // Assert that the returned StepResult contains the correct content
         Assert.HasCount(1, stepResult.Outputs);
@@ -429,6 +409,81 @@ public class PipelineStepTest
 
         using var pipelineStep = new PipelineStep("my_step", [], inputConfigs, [], processMock);
 
+        Assert.AreEqual(StepState.Pending, pipelineStep.State);
+
+        var stepResult = Task.Run(() => pipelineStep.Run(pipelineContext, CancellationToken.None)).GetAwaiter().GetResult();
+
+        Assert.AreEqual(StepState.Failed, pipelineStep.State);
+        Assert.AreEqual(0, processMock.NumberOfRunInvokations, "Process Run method was invoked.");
+    }
+
+    [TestMethod]
+    public void StepRunFailsIfInputDataIsOfWrongType()
+    {
+        var inputConfigs = new List<InputConfig>
+        {
+            NewInputConfig("step_01", "stringData", "stringData"),
+            NewInputConfig("step_01", "intData", "intData"),
+            NewInputConfig("step_01", "not_boolean_data", "booleanData"),
+            NewInputConfig("step_01", "doubleData", "doubleData"),
+        };
+        var stepStepResult01 = new StepResult()
+        {
+            Outputs = new Dictionary<string, StepOutput>
+            {
+                { "stringData", new StepOutput { Action = new HashSet<OutputAction>(), Data = "some data from step 01" } },
+                { "intData", new StepOutput { Action = new HashSet<OutputAction>(), Data = 123456 } },
+                { "not_boolean_data", new StepOutput { Action = new HashSet<OutputAction>(), Data = "expected to be a boolean but is a string" } },
+                { "doubleData", new StepOutput { Action = new HashSet<OutputAction>(), Data = 123.456 } },
+            },
+        };
+        var pipelineContext = new PipelineContext()
+        {
+            StepResults = new Dictionary<string, StepResult>()
+            {
+                { "step_01", stepStepResult01 },
+            },
+        };
+        var processMock = new MockPipelineProcessManyDifferentInputTypesInput([]);
+        using var pipelineStep = new PipelineStep("my_step", [], inputConfigs, [], processMock);
+        Assert.AreEqual(StepState.Pending, pipelineStep.State);
+
+        var stepResult = Task.Run(() => pipelineStep.Run(pipelineContext, CancellationToken.None)).GetAwaiter().GetResult();
+
+        Assert.AreEqual(StepState.Failed, pipelineStep.State);
+        Assert.AreEqual(0, processMock.NumberOfRunInvokations, "Process Run method was invoked.");
+    }
+
+    [TestMethod]
+    public void StepRunFailsIfInputDataIsOfWrongTypeForArray()
+    {
+        var inputConfigs = new List<InputConfig>
+        {
+            NewInputConfig("step_01", "intData", "intData"),
+            NewInputConfig("step_01", "booleanData", "booleanData"),
+            NewInputConfig("step_01", "doubleData", "doubleData"),
+            NewInputConfig("step_01", "string_data", "stringData"),
+            NewInputConfig("step_01", "not_string_data", "stringData"),
+        };
+        var stepStepResult01 = new StepResult()
+        {
+            Outputs = new Dictionary<string, StepOutput>
+            {
+                { "not_string_data", new StepOutput { Action = new HashSet<OutputAction>(), Data = 42 } },
+                { "intData", new StepOutput { Action = new HashSet<OutputAction>(), Data = 123456 } },
+                { "booleanData", new StepOutput { Action = new HashSet<OutputAction>(), Data = false } },
+                { "doubleData", new StepOutput { Action = new HashSet<OutputAction>(), Data = 123.456 } },
+            },
+        };
+        var pipelineContext = new PipelineContext()
+        {
+            StepResults = new Dictionary<string, StepResult>()
+            {
+                { "step_01", stepStepResult01 },
+            },
+        };
+        var processMock = new MockPipelineProcessManyDifferentInputTypesInput([]);
+        using var pipelineStep = new PipelineStep("my_step", [], inputConfigs, [], processMock);
         Assert.AreEqual(StepState.Pending, pipelineStep.State);
 
         var stepResult = Task.Run(() => pipelineStep.Run(pipelineContext, CancellationToken.None)).GetAwaiter().GetResult();
