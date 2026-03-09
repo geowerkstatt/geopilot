957d43475d596f2a69649353edb9c3586ed03c80
Nenad Stevic <nenad.stevic@geowerkstatt.ch>
2026-03-09 11:08:11 +0100

    Pass nonce from header to MUI via cache provider, so styles are properly attributed and can load

--- src/Geopilot.Frontend/src/main.tsx
+++ src/Geopilot.Frontend/src/main.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import createCache from "@emotion/cache";
+import { CacheProvider } from "@emotion/react";
 import "./index.css";
 import "./assets/fonts/fonts.css";
 import { AppContext } from "./appContext.tsx";
 
+const nonce = document.querySelector<HTMLMetaElement>('meta[property="csp-nonce"]')?.nonce;
+const emotionCache = createCache({ key: "css", nonce });
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AppContext />
+    <CacheProvider value={emotionCache}>
+      <AppContext />
+    </CacheProvider>
   </React.StrictMode>,
 );
