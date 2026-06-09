d80b0ed89e5f1e875a8d7479fd8cd4e1e3a888d7
Lukas Oldani <lukas.oldani@geowerkstatt.ch>
2026-06-09 16:16:18 +0200

    chore: update comment

--- src/Geopilot.Api/Pipeline/Process/Hop/HopArgs.cs
+++ src/Geopilot.Api/Pipeline/Process/Hop/HopArgs.cs
@@ -3,6 +3,6 @@
 /// <summary>
 /// Per-run arguments serialized to <c>args.json</c> for the Hop worker.
 /// </summary>
-/// <param name="Pipeline">Name of the Hop pipeline or workflow file (<c>.hpl</c>/<c>.hwf</c>) relative to the worker's pipeline-definition directory.</param>
+/// <param name="Pipeline">Absolute path of the Hop pipeline or workflow file (<c>.hpl</c>/<c>.hwf</c>) within the worker, passed straight to <c>hop-run --file</c>.</param>
 /// <param name="Parameters">Key-value pairs forwarded as <c>-p</c> parameters to <c>hop-run</c>. May be empty.</param>
 internal sealed record HopArgs(string Pipeline, IReadOnlyDictionary<string, string> Parameters);
