1954ac2e2bb2b67213ec7e1ac9e62e5416213c39
Dominic Burger <dominic.burger@geowerkstatt.ch>
2026-07-30 14:59:22 +0200

    Fix horizontal layout shift from scrollbar

--- src/Geopilot.Frontend/src/pages/footer/footer.tsx
+++ src/Geopilot.Frontend/src/pages/footer/footer.tsx
@@ -11,45 +11,47 @@ const Footer = () => {
   const marginLeft = isAdminRoute ? "250px" : "0";
 
   return (
-    <Stack
-      direction="row"
-      pb={1}
-      px={3}
-      sx={{
-        alignItems: "center",
-        justifyContent: "center",
-        flexWrap: "wrap",
-        marginLeft: { xs: "0", md: marginLeft },
-      }}
-      className="footer">
-      <Button
-        size={isXs ? "small" : "medium"}
-        variant="text"
-        data-cy="home-nav"
-        label="geopilot"
-        onClick={() => navigateTo("/")}
-      />
-      <Button
-        size={isXs ? "small" : "medium"}
-        variant="text"
-        data-cy="privacy-policy-nav"
-        label="privacyPolicy"
-        onClick={() => navigateTo("/privacy-policy")}
-      />
-      <Button
-        size={isXs ? "small" : "medium"}
-        variant="text"
-        data-cy="imprint-nav"
-        label="imprint"
-        onClick={() => navigateTo("/imprint")}
-      />
-      <Button
-        size={isXs ? "small" : "medium"}
-        variant="text"
-        data-cy="about-nav"
-        label="about"
-        onClick={() => navigateTo("/about")}
-      />
+    <Stack sx={{ paddingLeft: "calc(100vw - 100%)" }}>
+      <Stack
+        direction="row"
+        pb={1}
+        px={3}
+        sx={{
+          alignItems: "center",
+          justifyContent: "center",
+          flexWrap: "wrap",
+          marginLeft: { xs: "0", md: marginLeft },
+        }}
+        className="footer">
+        <Button
+          size={isXs ? "small" : "medium"}
+          variant="text"
+          data-cy="home-nav"
+          label="geopilot"
+          onClick={() => navigateTo("/")}
+        />
+        <Button
+          size={isXs ? "small" : "medium"}
+          variant="text"
+          data-cy="privacy-policy-nav"
+          label="privacyPolicy"
+          onClick={() => navigateTo("/privacy-policy")}
+        />
+        <Button
+          size={isXs ? "small" : "medium"}
+          variant="text"
+          data-cy="imprint-nav"
+          label="imprint"
+          onClick={() => navigateTo("/imprint")}
+        />
+        <Button
+          size={isXs ? "small" : "medium"}
+          variant="text"
+          data-cy="about-nav"
+          label="about"
+          onClick={() => navigateTo("/about")}
+        />
+      </Stack>
     </Stack>
   );
 };
