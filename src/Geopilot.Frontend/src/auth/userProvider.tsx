aa981a37315204eaded9ba4c0c7636f445360cdc
Nenad Stevic <nenad.stevic@geowerkstatt.ch>
2026-09-16 21:24:48 +0200

    Reset attempt to 0 for better UX

--- src/Geopilot.Frontend/src/auth/userProvider.tsx
+++ src/Geopilot.Frontend/src/auth/userProvider.tsx
@@ -20,11 +20,15 @@ export const UserProvider: FC<PropsWithChildren> = ({ children }) => {
         },
         errorMessageLabel: attempt === 0 ? "userInfoLoadingError" : undefined,
       })
-        .then(setUser)
+        .then(user => {
+          setUser(user);
+          setAttempt(0);
+        })
         .catch(error => {
           if (error instanceof ApiError && (error.status === 401 || error.status === 403)) {
             auth.signoutSilent();
             setUser(null);
+            setAttempt(0);
           } else {
             retryTimer.current = setTimeout(() => setAttempt(current => current + 1), 5000);
           }
@@ -38,6 +42,7 @@ export const UserProvider: FC<PropsWithChildren> = ({ children }) => {
       fetchUserInfo(auth);
     } else if (auth && !auth.isLoading) {
       setUser(null);
+      setAttempt(0);
     }
     return () => clearTimeout(retryTimer.current);
   }, [auth, auth?.isAuthenticated, auth?.isLoading, fetchUserInfo]);
